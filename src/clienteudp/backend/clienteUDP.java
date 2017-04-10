a5e79f65a124b84230072ae8a053b710fa2becec
dfSabogal <david.sabogal1@gmail.com>
2017-04-09 23:36:41 -0500

    envioarchivosCompleto

--- src/clienteudp/backend/clienteUDP.java
+++ src/clienteudp/backend/clienteUDP.java
@@ -2,6 +2,7 @@ package clienteudp.backend;
 
 import java.io.*;
 import java.net.*;
+import java.security.MessageDigest;
 import java.util.Date;
 
 public class clienteUDP {
@@ -29,12 +30,13 @@ public class clienteUDP {
 	}
 
 	public void setPuertoServidor(int puertoServidor) throws Exception {
-			this.puertoServidor = puertoServidor;
+		this.puertoServidor = puertoServidor;
 	}
 
 	public void establecerConexion() throws Exception{
-	
+
 		socketCliente = new DatagramSocket(9876);
+		socketCliente.setReceiveBufferSize(64000);
 	}
 
 	public void enviarPaquetes() throws Exception{
@@ -51,14 +53,65 @@ public class clienteUDP {
 		}
 	}
 
-	public void enviarArchivo(File archivoAEnviar) {
-		// TODO Auto-generated method stub
+	public void enviarArchivo(File archivoAEnviar) throws Exception{
 		System.out.println("envio archivo");
+		// calculate hash
+
+		MessageDigest md = MessageDigest.getInstance("SHA1");
+	    FileInputStream fis = new FileInputStream(archivoAEnviar);
+	    byte[] dataBytes = new byte[1024];
+
+	    int nread = 0;
+
+	    while ((nread = fis.read(dataBytes)) != -1) {
+	      md.update(dataBytes, 0, nread);
+	    };
+
+	    byte[] mdbytes = md.digest();
+
+	    //convert the byte to hex format
+	    StringBuffer sb = new StringBuffer("");
+	    for (int i = 0; i < mdbytes.length; i++) {
+	    	sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+	    }
+
+	    System.out.println("Digest(in hex format):: " + sb.toString());
+
+		//send fileInfo
+	    byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+		String sentence = "ARCHIVO:"+archivoAEnviar.getName()+":"+sb.toString()+":"+archivoAEnviar.length()+":";
+		sendData = sentence.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipServidor, puertoServidor);
+		socketCliente.send(sendPacket);
+		System.out.println("envio info: "+sentence);
+
+		// send file
+		sendData =new byte[(int) archivoAEnviar.length()];
+		FileInputStream fis2 = new FileInputStream(archivoAEnviar);
+		BufferedInputStream bis = new BufferedInputStream(fis2);
+		bis.read(sendData,0,sendData.length);
+		System.out.println("longitud archivo: "+sendData.length);
+		for (int i = 0; i < sendData.length; i++) {
+			
+			if((sendData.length-i)>= 64000){
+				
+				sendPacket = new DatagramPacket(sendData, i,64000, ipServidor, puertoServidor);
+				socketCliente.send(sendPacket);
+				i+=(64000-1);
+				System.out.println("enviados "+i+" bytes del archivo...");
+			}
+			else{
+				sendPacket = new DatagramPacket(sendData, i,sendData.length-i, ipServidor, puertoServidor);
+				socketCliente.send(sendPacket);
+				System.out.println("enviado ultimo paquete de"+(i+(sendData.length-i))+" bytes del archivo.");
+				i+=(64000-1);
+
+
+			}
+			
+		}
+
 	}
-
-
-
-
-
 
 }
